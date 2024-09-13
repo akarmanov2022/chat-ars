ed35102d409a9206c631aab661af298cd4da230a
akarmanov <mrmorris853@gmail.com>
2024-09-13 21:26:08 +0700

    Initial commit

---
+++ src/main/java/net/akarmanov/chatars/BootApplication.java
@@ -0,0 +1,13 @@
+package net.akarmanov.chatars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkchatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AkchatApplication.class, args);
+  }
+
+}
